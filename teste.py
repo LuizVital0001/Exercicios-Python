261ef8f7bcc705dab5f8435df6245f7ac1a9a514
Vital <luizhenrique.vital9@gmail.com>
2025-09-12 16:55:02 -0300

    Feat: criando um algoritimo simples que recebe dois valores e depois mostra sua soma

---
+++ teste.py
@@ -0,0 +1,7 @@
+# 1.Criando um programa para receber dois valores e em seguida, exibir o valor da soma.
+'''
+num1 = int(input( "digite o primeiro valor: "))
+num2 = int(input("digite o segundo valor: "))
+soma = num1 + num2
+print(soma)
+'''
