b17473b043d479d6aa54fe14b4e16319e47cc788
Vital <luizhenrique.vital9@gmail.com>
2025-09-14 12:28:01 -0300

    feat: implementa verificador de maioridade (ex. #01)
    Sistema que verifica se uma pessoa é maior de idade:
    - Cria variável idade com valor inteiro
    - Verifica se idade é maior ou igual a 18
    - Imprime Maior de idade ou Menor de idade conforme condição

---
+++ exercicios 2.py
@@ -0,0 +1,10 @@
+#01 Crie uma variável chamada "idade" e atribua um valor inteiro a ela. 
+#Verifique se a idade é maior ou igual a 18 e imprima "Maior de idade" ou "Menor de idade" de acordo com a condição. 
+
+idade = 18
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+
