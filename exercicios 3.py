10e79e37da1e60d4d3558d8908c32dc0289ccf85
Vital <luizhenrique.vital9@gmail.com>
2025-09-15 21:42:17 -0300

    feat: implementa sistema de validação de usuário e senha (ex. #2)
    Sistema que verifica se senha é diferente do nome de usuário:
    - Solicita nome de usuário e senha repetidamente até validação
    - Compara se usuário e senha são idênticos
    - Exibe mensagem de erro específica para credenciais iguais
    - Fornece mensagem de boas-vindas personalizada quando válido
    - Utiliza loop infinito até receber credenciais válidas

--- exercicios 3.py
+++ exercicios 3.py
@@ -1,4 +1,5 @@
 #1 Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+'''
 while True:
     try:
         nota = float(input("Digite uma nota entre 0 e 10: "))
@@ -11,7 +12,22 @@ while True:
 
     except ValueError:
         print("Valor inválido! Digite apenas números.") 
-
+'''
 #2 Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha 
-# igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações. 
-    
+#igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+while True: 
+    usuario = input("Digite o nome do úsuario: ")
+    senha = input("Digite sua senha: ")
+    if usuario == senha:
+        print("O usuário e a senha não podem ser idênticos!")
+    else:
+        print(f"Bem vindo de volta {usuario}!")
+        break
+
+#3 Faça um programa que leia e valide as seguintes informações:  
+#a) Nome: maior que 3 caracteres;  
+#b) Idade: entre 0 e 150;  
+#c) Salário: maior que zero;  
+#d) Sexo: 'f' ou 'm';  
+#e) Estado Civil: 's', 'c', 'v', 'd'
